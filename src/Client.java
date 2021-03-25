2cf4143f86d40d34e277b32cccb7ef23238fb387
Kaan Kulak <kaankulak00@gmail.com>
2021-03-25 21:48:31 +0300

    new functionalities

--- src/Client.java
+++ src/Client.java
@@ -4,30 +4,29 @@ import java.net.*;
 
 public class Client {
     private Socket s;
-    private int serverPort; //my configuration works on port 25(SMTP)
+    private int serverPort; //configuration works on port 25(SMTP) with Postfix
     private String serverIpAddress;
 
-    BufferedReader in; //reads everything in a file to a variable
+    BufferedReader in;
     PrintWriter out;
 
-    String inData;
-    String outData;
+    Scanner inputs = new Scanner(System.in);
+    Scanner commands = new Scanner(System.in);
 
-    //constructor for client
     public Client() {
         clientStart();
     }
 
     private void clientStart(){
-        serverIpAddress = "127.0.0.1"; //localhost
-        serverPort = 25; //25 for SMTP
-        System.out.println("Server IP   : "+serverIpAddress);
-        System.out.println("Server PORT : "+serverPort);
+
+        selectServerIpAddress(inputs);
+        selectPort(inputs);
+        printServerDetails(serverIpAddress,serverPort);
+
         try{
-            s = new Socket(serverIpAddress,serverPort); //creating a connection point(socket)
+            s = new Socket(serverIpAddress,serverPort); //creating a connection
             System.out.println("\nConnected!");
-            Scanner inputs = new Scanner(System.in);
-            clientService(inputs);
+            clientService(commands); //running commands to send e-mail
         }
         catch (Exception e){
             System.out.println("Error : "+e);
@@ -42,22 +41,18 @@ public class Client {
         }
     }
 
-    //sending commands through telnet
     public void clientService(Scanner inputs){
         try {
-            String var;
-
             in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            out = new PrintWriter(s.getOutputStream(),true); //autoflush to clean buffer after inputs.
+            out = new PrintWriter(s.getOutputStream(),true); //flush to clean buffer after inputs.
 
             //sending mail
             sendMail(in,out,inputs);
 
-
             in.close();
             out.close();
 
-            System.out.println("Your e-mail successfully sent.");
+            System.out.println("\nYour e-mail successfully sent.");
         }
         catch (Exception e){
             System.out.println("Error"+e);
@@ -67,31 +62,48 @@ public class Client {
     private void sendMail(BufferedReader in,PrintWriter out, Scanner inputs) throws IOException {
         String var;
 
-        //MAIL FROM: Implementation for SMTP protocol
-        System.out.println("MAIL FROM: ");
+        //SMTP protocol commands to send e-mail
+
+        System.out.print("MAIL FROM: ");
         var = inputs.nextLine();
         System.out.println(in.readLine());
         out.println("MAIL FROM: "+var);
         System.out.println(in.readLine());
 
-        System.out.println("RCPT TO: ");
+        System.out.print("RCPT TO: ");
         var = inputs.nextLine();
         out.println("RCPT TO: "+var);
         System.out.println(in.readLine());
 
-        out.println("data");
+        out.println("DATA");
         System.out.println(in.readLine());
 
-        System.out.println("Your message: ");
+        System.out.print("DATA: ");
         var = inputs.nextLine();
         out.println(var);
         out.println(".");
 
-        System.out.println("SUBJECT: ");
+        System.out.print("SUBJECT: ");
         var = inputs.nextLine();
 
         out.println("SUBJECT: "+var);
         System.out.println(in.readLine());
         out.println("quit");
+
+    }
+
+    private void selectServerIpAddress(Scanner inputs) {
+        System.out.print("IP Address(can be localhost): ");
+        this.serverIpAddress = inputs.nextLine();
+    }
+
+    private void selectPort(Scanner inputs){
+        System.out.print("Server PORT(25,465,587): ");
+        this.serverPort = inputs.nextInt();
+    }
+
+    private void printServerDetails(String serverIpAddress,int serverPort){
+        System.out.println("\nServer IP   : "+serverIpAddress);
+        System.out.println("Server PORT : "+serverPort);
     }
 }
